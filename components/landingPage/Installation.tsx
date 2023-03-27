380442ab175be543c67de3b05e0a3c857220746c
Majura Anthony Gaudence <andersonmia1968@gmail.com>
2023-03-27 21:08:54 +0200

    [feat]: landing page until services part fixing margin

---
+++ components/landingPage/Installation.tsx
@@ -0,0 +1,17 @@
+
+const installation = ()=>{
+    return(
+        <div>
+            <div className="flex">
+                <img src="image/installation.png"></img>
+                <div>
+                    <h1 className="font-extrabold text-3xl mb-4">We install high sensitivity tools at the mining site </h1>
+                    <p className="text-[#3F3F3F]">Here's the services that we offer to you, to ensure both your company's products and employees security</p>
+                    <button className="rounded-full bg-[#5A3FFF] text-white">Learn more</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default installation
