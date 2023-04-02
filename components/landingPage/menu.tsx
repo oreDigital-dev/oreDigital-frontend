68b8b806d0359f576e3774ea424b10da978b9c1c
Ingabire Divine <divineingabire69@gmail.com>
2023-04-02 19:48:33 +0200

    added impact page

--- components/landingPage/menu.tsx
+++ components/landingPage/menu.tsx
@@ -74,3 +74,21 @@ export const partners = [
           path:"image/cousine.png"
      }
 ]
+export const impactContent = [
+     {
+         image:"image/incident.png",
+         title:"Poweful Incident reporting system at the site",
+         description:"Here’s  the services that we offer to you, to ensure both your company’s products and employees security"
+     },
+     {
+          image:"image/transaction.png",
+          title:"Poweful and Extra secure transaction recording",
+          description:"Here’s  the services that we offer to you, to ensure both your company’s products and employees security"
+      },
+      {
+          image:"image/performance.png",
+          title:"Improvement in company’ performance stats.",
+          description:"Here’s  the services that we offer to you, to ensure both your company’s products and employees security"
+      }
+
+ ]
--- components/landingPage/welcomePage.tsx
+++ components/landingPage/welcomePage.tsx
@@ -4,9 +4,11 @@ import Footer from './footer'
 import HomePage from './homePage'
 import Installation from './installation'
 import Navbar from './navbar'
+import OurImpact from './ourImpact'
 import Partners from './partners'
 import Process from './process'
 import ServicesC from './servicesContent'
+import Team from './team'
 function WelcomePage() {
   return (
     <div>
@@ -15,6 +17,8 @@ function WelcomePage() {
       <ServicesC />
       <Installation />
       <Process />
+      <Team />
+      <OurImpact />
       <AboutUs />
       <Partners />
       <Footer />
